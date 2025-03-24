a120499dd3db6c9384002c346c963ab652dba5b9
Blakenb <blakenb87@gmail.com>
2025-03-23 19:28:15 -0500

    made some changes for my vite.config to work with render

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  envDir: 'environment',
+  envDir: "environment",
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@pages': '/src/pages',
-      '@api': '/src/api',
-      '@interfaces': '/src/interfaces',
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@api": "/src/api",
+      "@interfaces": "/src/interfaces",
     },
   },
   server: {
-    host: '0.0.0.0', // Ensures it binds to all interfaces
-    port: 5173, // Default Vite port, change if needed
+    host: "0.0.0.0", // Ensures it binds to all interfaces
+    port: 5173, // Default Vite port
     strictPort: true, // Ensures Vite uses the specified port
-    allowedHosts: ['your render link here'], // Allow Render host
+    allowedHosts: ["github-candidate-search-13-2.onrender.com"], // Allow Render host
   },
 });
